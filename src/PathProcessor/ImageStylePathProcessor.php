7b0132ba4cdc726ce6392c63fb9b4819586bb26c
Daniel Wirz <d.wirz@iqual.ch>
2020-11-19 07:58:35 +0100

    automatic creation of images an request

---
+++ src/PathProcessor/ImageStylePathProcessor.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Drupal\pagedesigner_responsive_images\PathProcessor;
+
+use Drupal\Core\PathProcessor\InboundPathProcessorInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
+/**
+ * Alter image style paths to accept slashes in params.
+ */
+class ImageStylePathProcessor implements InboundPathProcessorInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function processInbound($path, Request $request) {
+
+
+    if (strpos($path, '/sites/default/files/styles/') === 0) {
+
+      if (!file_exists($_SERVER['DOCUMENT_ROOT'] . $request->getPathInfo())) {
+        $pathParts = explode('/', str_replace('/sites/default/files/styles/', '', $request->getPathInfo()));
+        $imageStyle = $pathParts[0];
+        array_shift($pathParts);
+        $uriBase = array_shift($pathParts);
+        $imgUri = $uriBase . '://' . implode('/', $pathParts);
+
+        $style = \Drupal::entityTypeManager()
+          ->getStorage('image_style')
+          ->load($imageStyle);
+
+        $imgUrl = $style->buildUrl($imgUri);
+        $response = new RedirectResponse($imgUrl);
+        $response->send();
+      }
+    }
+    return $path;
+  }
+
+}
